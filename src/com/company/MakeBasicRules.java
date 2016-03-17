8353a210c8d862beed54c9bbd27088384b075710
Kuzzmich <kuzzmich204@mail.ru>
2016-03-17 14:25:24 +0300

    Реализованы все классы. Осталось только формирование базовой матрицы доступа

---
+++ src/com/company/MakeBasicRules.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.io.*;
+
+/**
+ * Created by Алексей on 13.03.2016.
+ */
+public class MakeBasicRules {
+    public void readAndMake(){
+        int maxLineNum=20;
+        int lineNum=0;
+        String srchString=null;
+        String anchor="////";
+        try {
+            LineNumberReader lnr = new LineNumberReader(new BufferedReader(new FileReader("Student.java")));
+            for (int i=0;i<maxLineNum; i++){
+                String tmpString = lnr.readLine();
+                if(tmpString == null)
+                    break;
+
+                if(tmpString.indexOf(anchor) != -1)
+                {
+                    srchString=tmpString;
+                    break;
+                }
+            }
+
+            srchString=srchString.replaceAll("\\////|\\--", "");    // Топорный метод, но удаляемые
+            srchString=srchString.trim();                           // элементы изначально известны
+
+            FileWriter fw=new FileWriter("Matrix.txt", true);
+            fw.append(srchString+System.getProperty("line.separator"));
+            fw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

6cfca192a2c941ad1970719a0d4fafbd27b58117
Kuzzmich <kuzzmich204@mail.ru>
2016-03-17 14:26:11 +0300

    формирование базовой матрицы

--- src/com/company/MakeBasicRules.java
+++ src/com/company/MakeBasicRules.java
@@ -16,7 +16,7 @@ public class MakeBasicRules {
             for (int i=0;i<maxLineNum; i++){
                 String tmpString = lnr.readLine();
                 if(tmpString == null)
-                    break;s
+                    break;
 
                 if(tmpString.indexOf(anchor) != -1)
                 {
